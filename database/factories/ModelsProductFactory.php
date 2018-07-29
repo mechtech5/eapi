9fc00f394e4f0169e86b66de0ba57ed1f3a0780e
Ayush Likhar <neontechhead@gmail.com>
2018-07-30 00:06:32 +0530

    products and reviews seeded

--- database/factories/ModelsProductFactory.php
+++ database/factories/ModelsProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100,1000),
+        'stock' => $faker->numberBetween(1,1000),
+        'discount' => $faker->numberBetween(10,90)
+        
     ];
 });
--- database/factories/ModelsReviewFactory.php
+++ database/factories/ModelsReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+            return Product::all()->random();
+        },
+        'customer' => $faker->word,
+        'review' => $faker->paragraph,
+        'rating' => $faker->numberBetween(1,5)
     ];
 });
