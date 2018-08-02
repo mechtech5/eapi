e0b9b7f03c1e2ac4c0462e4ccc2829dbecbecf01
Ayush Likhar <neontechhead@gmail.com>
2018-08-03 00:25:38 +0530

    Update Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
+	protected $fillable = [
+		'name','detail','stock','price','discount'
+	];
+
+  public function reviews()
+  {
+      return $this->hasMany(Review::class);
+  }
 }
